d8d3dccdd1f5d650923a9162905cbaa9014b66aa
kalekale <lehikoinen.kalle@gmail.com>
2017-02-28 19:27:52 +0200

    uses manifold deferred

--- src/clj/rekry/handler.clj
+++ src/clj/rekry/handler.clj
@@ -9,19 +9,11 @@
             [clojure.java.io :as io]
             [clojure.data.json :as json]
             [org.httpkit.client :as http]
-            [config.core :refer [env]]))
+            [config.core :refer [env]]
+            [manifold.deferred :as d]))
 
 
 (def auth-str (str "?" "client_id=" (env :client-id) "&" "client_secret=" (env :client-secret)))
-
-(extend-type clojure.core.async.impl.channels.ManyToManyChannel
-  StreamableResponseBody
-  (write-body-to-stream [channel response output-stream]
-    (a/go (with-open [writer (io/writer output-stream)]
-                (a/loop []
-                  (when-let [msg (a/<! channel)]
-                    (doto writer (.write msg) (.flush))
-                    (recur)))))))
 
 (defn parse-link-string [link-str]
   (subs link-str 1 (dec (count link-str))))
@@ -49,14 +41,36 @@
    (for [page-number (range 1 (inc page-count))]
      (str url "&per_page=100" "&page=" page-number))))
 
+(defn repo-downloads [entry clojars-res]
+  (:downloads
+   (first (filter #(= (:name entry) (:jar_name %))
+                  clojars-res))))
+
+(defn link-downloads-to-repos [repos clojars-body]
+  (mapv #(assoc % :downloads (repo-downloads % clojars-body)) repos))
+
+(defn add-downloads [org repos]
+  (d/chain (http/get (str "https://clojars.org/api/groups/" org))
+           parse-body-from-req
+           (partial link-downloads-to-repos repos)))
+
+
+(defn rest-pages [first-page-res base-url]
+  (if (has-link-headers? first-page-res)
+    (let [page-count (last-page-number first-page-res)
+          urls (create-page-urls base-url page-count)]
+      (apply d/zip (map #(http/get %) urls)))
+    (seq [first-page-res])))
+
+(defn count-objects [promises]
+  (reduce #(+ %1 (count (json/read-json (:body %2))))
+          0
+          promises))
+
 (defn all-pages [base-url]
-  (let [first-page (str base-url "&per_page=100" "&page=1")
-        res (http/get first-page)]
-    (if (has-link-headers? @res)
-      (let [page-count (last-page-number @res)
-            urls (create-page-urls base-url page-count)]
-        (doall (map http/get urls)))
-      [res])))
+  (let [first-page (str base-url "&per_page=100" "&page=1")]
+    (d/chain (http/get first-page)
+             #(rest-pages % base-url))))
 
 (defn contr-url [repo]
   (str (:contributors_url repo) auth-str))
@@ -64,57 +78,41 @@
 (defn commit-url [repo]
   (str (clojure.string/replace (:commits_url repo)  #"\{/sha\}" "") auth-str))
 
-(defn count-objects [promises]
-  (reduce #(+ %1 (count (json/read-json (:body @%2))))
-          0
-          promises))
+(defn repo-data [repo]
+  (d/let-flow [contributors (all-pages (contr-url repo))
+               commits      (all-pages (commit-url repo))
+               name         (:name repo)
+               language     (:language repo)
+               stargazers   (:stargazers-count repo)]
+    {:name             name
+     :language         language
+     :stargazers-count stargazers
+     :commits          (count-objects commits)
+     :contributors     (count-objects contributors)}))
 
-(defn repo-data-to-chan [repo ch]
-  (let [name (get repo :name)
-        language (get repo :language)
-        stargazers-count (get repo :stargazers_count)
-        cont-promises (all-pages (contr-url repo))
-        commit-promises (all-pages (commit-url repo))
-        contributors (count-objects cont-promises)
-        commits (count-objects commit-promises)]
-    (go (a/>! ch {:name name
-                  :language language
-                  :stargazers-count stargazers-count
-                  :contributors contributors
-                  :commits commits}))))
+(defn data-to-repos [repos]
+  (apply d/zip (map #(repo-data %) repos)))
+
+(defn parse-body-from-req [req]
+  (json/read-json (:body req)))
 
 (defn repos-by-org [org]
   (http/get (str "https://api.github.com/orgs/" org "/repos" auth-str "&page=1" "&per_page=100")))
 
-(defn repo-downloads [entry clojars-res]
-  (:downloads
-   (first (filter #(= (:name entry) (:jar_name %))
-                  clojars-res))))
-
-(defn add-downloads [body]
-  (let [res (json/read-json (:body @(http/get "https://clojars.org/api/groups/metosin")))]
-    (mapv
-     #(assoc % :downloads (repo-downloads % res))
-     body)))
-
-(defn async-repos-handler []
+(defn d-handler []
   (fn [req res raise]
-    (let [ch1 (a/chan)
-          ch2 (a/chan)
-          body (atom [])]
-      (res {:status 200, :headers {}, :body ch2})
-      (let [repos (repos-by-org (:query-string req))]
-        (doseq [repo (json/read-json (:body @repos))]
-          (future (repo-data-to-chan repo ch1)))
-        (doseq [_ (json/read-json (:body @repos))]
-          (swap! body conj (a/<!! ch1)))
-        (swap! body add-downloads)
-        (a/>!! ch2 (json/write-str @body))
-        (a/close! ch2)))))
+    (res
+     (d/chain
+      (repos-by-org (:query-string req))
+      parse-body-from-req
+      data-to-repos
+      (partial add-downloads (:query-string req))
+      #(hash-map :status 200 :body (json/write-str %))))))
 
 (defroutes routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
   (resources "/")
-  (GET "/repos" [] (async-repos-handler)))
+  (GET "/repos" [] (d-handler)))
 
-(def handler routes)
+
+(def handler #'routes)
